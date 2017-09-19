355481940618681b6df84cd646594de38847af5f
AeonLucid <aeonlucid@outlook.com>
2017-09-19 09:58:04 +0200

    Add project files.

---
+++ Pingu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pingu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
